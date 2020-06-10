30495f39edc75c86281218e8b23d3701259d37e1
foreverszz <1414122337@qq.com>
2020-06-10 11:10:38 +0800

    提交文件

---
+++ leyouDemo/leyou-auth/leyou-auth-common/src/test/java/com/leyou/auth/test/JwtTest.java
@@ -0,0 +1,49 @@
+package com.leyou.auth.test;
+
+import com.leyou.common.pojo.UserInfo;
+import com.leyou.common.utils.JwtUtils;
+import com.leyou.common.utils.RsaUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+public class JwtTest {
+
+    private static final String pubKeyPath = "C:\\tmp\\rsa\\rsa.pub";
+
+    private static final String priKeyPath = "C:\\tmp\\rsa\\rsa.pri";
+
+    private PublicKey publicKey;
+
+    private PrivateKey privateKey;
+
+    @Test
+    public void testRsa() throws Exception {
+        RsaUtils.generateKey(pubKeyPath, priKeyPath, "234");
+    }
+
+    @Before
+    public void testGetRsa() throws Exception {
+        this.publicKey = RsaUtils.getPublicKey(pubKeyPath);
+        this.privateKey = RsaUtils.getPrivateKey(priKeyPath);
+    }
+
+    @Test
+    public void testGenerateToken() throws Exception {
+        // 生成token
+        String token = JwtUtils.generateToken(new UserInfo(20L, "jack"), privateKey, 5);
+        System.out.println("token = " + token);
+    }
+
+    @Test
+    public void testParseToken() throws Exception {
+        String token = "eyJhbGciOiJSUzI1NiJ9.eyJpZCI6MjAsInVzZXJuYW1lIjoiamFjayIsImV4cCI6MTU4Nzk1ODQ4NH0.T3o1uV9rozz-YjrZmrt5TgyTtmo2-EYrkMADyh4xD4jYk0wdyheBFE0I5FUaI2E3vOjWxbboUlQrLSCN-dmXfbnUjKEkD0Yj15u7DuwKrVrcTzJ_Diq43O5RApQKm_iw-Koxx3T2kZ8nEcJ95ajCb-bVhCJ9Lut_HJJH3ImjmEY";
+
+        // 解析token
+        UserInfo user = JwtUtils.getInfoFromToken(token, publicKey);
+        System.out.println("id: " + user.getId());
+        System.out.println("userName: " + user.getUsername());
+    }
+}
